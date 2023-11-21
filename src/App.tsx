cda4063905f5c0b520d2984bccf348f280584690
Juan Ludevid <jmludevid@hotmail.com>
2023-11-22 04:49:29 +1100

    update Item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 12, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 12, packed: true },
+// ];
 
+type ItemType = {
+  id: number;
+  description: string;
+  quantity: number;
+  packed: boolean;
+};
 interface ItemProps {
-  item: {
-    id: number;
-    description: string;
-    quantity: number;
-    packed: boolean;
-  };
+  item: ItemType;
 }
 
 export default function App() {
+  const [items, setItems] = useState<ItemProps[]>([]);
+
+  function handleAddItems(item: ItemType) {
+    setItems((items) => [...items, { item }]);
+  }
+  function handleDeleteItem(id: number) {
+    setItems((items) => items.filter((item) => item.item.id !== id));
+  }
+  function handleToggleItem(id: number) {
+    setItems((items) =>
+      items.map((item) =>
+        item.item.id === id
+          ? { ...item, item: { ...item.item, packed: !item.item.packed } }
+          : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -30,12 +52,18 @@ function Logo() {
   return <h1>🪅🪅🪅 Far Away 🪅🪅🪅</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }: { onAddItems: (item: ItemType) => void }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e: { preventDefault: () => void }) {
     e.preventDefault();
     if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
     setDescription("");
     setQuantity(1);
   }
@@ -63,25 +91,51 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+}: {
+  items: ItemProps[];
+  onDeleteItem: (id: number) => void;
+  onToggleItem: (id: number) => void;
+}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {items.map((item) => (
+          <Item
+            key={item.item.id}
+            item={item.item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }: ItemProps) {
+function Item({
+  item,
+  onDeleteItem,
+  onToggleItem,
+}: {
+  item: ItemType;
+  onDeleteItem: (id: number) => void;
+  onToggleItem: (id: number) => void;
+}) {
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.description} {item.quantity}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
