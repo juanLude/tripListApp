5834b7be129898f926a63e6284c5d18fb4d12270
Juan Ludevid <jmludevid@hotmail.com>
2023-11-22 22:16:43 +1100

    sort packing list

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -100,10 +100,24 @@ function PackingList({
   onDeleteItem: (id: number) => void;
   onToggleItem: (id: number) => void;
 }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems: ItemProps[] = [];
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.item.description.localeCompare(b.item.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.item.packed) - Number(b.item.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.item.id}
             item={item.item}
@@ -112,6 +126,13 @@ function PackingList({
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sory by input order</option>
+          <option value="description">Sory by description</option>
+          <option value="packed">Sory by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -139,10 +160,24 @@ function Item({
     </li>
   );
 }
-function Stats() {
+function Stats(items: { items: ItemProps[] }) {
+  if (!items.items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list.</em>
+      </p>
+    );
+  const numItems = items.items.length;
+  const numPacked = items.items.filter((item) => item.item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything ready to go!"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} ${percentage} (%)`}
+      </em>
     </footer>
   );
 }
